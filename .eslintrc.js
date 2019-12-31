a129a810c3a16a9206220e978ce6de86ab3f07c1
lizhiping <276340452@qq.com>
2019-12-31 16:56:21 +0800

    fix: TS 不需要prop-types

--- .eslintrc.js
+++ .eslintrc.js
@@ -45,5 +45,6 @@ module.exports = {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', 'ts', 'tsx'] }],
     'react/prefer-stateless-function': 0,
     'react/forbid-prop-types': [0, { forbid: ['object'] }],
+    'react/prop-types': 0,
   },
 };
