6dee058a5d10d9cc75156162bb566fc7a2a7774a
lizhiping <276340452@qq.com>
2020-02-19 17:10:41 +0800

    refactor: 修改webpack打包配置文件

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -108,10 +108,10 @@ module.exports = function(webpackEnv) {
               stage: 3,
             }),
             // px-to-rem
-            require('postcss-pxtorem')({
-              rootValue: 48, // 换算的基数
-              propList: ['*'],
-            }),
+            // require('postcss-pxtorem')({
+            //   rootValue: 48, // 换算的基数
+            //   propList: ['*'],
+            // }),
             // Adds PostCSS Normalize as the reset css with default options,
             // so that it honors browserslist config in package.json
             // which in turn let's users customize the target behavior as per their needs.
@@ -344,7 +344,7 @@ module.exports = function(webpackEnv) {
                 formatter: require.resolve('react-dev-utils/eslintFormatter'),
                 eslintPath: require.resolve('eslint'),
                 resolvePluginsRelativeTo: __dirname,
-                
+
               },
               loader: require.resolve('eslint-loader'),
             },
@@ -377,7 +377,7 @@ module.exports = function(webpackEnv) {
                 customize: require.resolve(
                   'babel-preset-react-app/webpack-overrides'
                 ),
-                
+
                 plugins: [
                   [
                     require.resolve('babel-plugin-named-asset-import'),
@@ -419,7 +419,7 @@ module.exports = function(webpackEnv) {
                 cacheDirectory: true,
                 // See #6846 for context on why cacheCompression is disabled
                 cacheCompression: false,
-                
+
                 // Babel sourcemaps are needed for debugging into node_modules
                 // code.  Without the options below, debuggers like VSCode
                 // show incorrect code and set breakpoints on the wrong lines.
