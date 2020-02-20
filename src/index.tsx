64593aff56991d16d2f76363949cb6c0f237bf03
lizhiping <276340452@qq.com>
2020-02-20 16:50:11 +0800

    style: 修改index.scss样式文件目录

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import { registerLocale } from './config/translation';
-import './index.scss';
+import 'assets/scss/index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import initStore from './config/store';
